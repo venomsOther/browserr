4c6242bff9fb053f425a9351e10cbe5b20352d62
eatmyvenom <jacobhagan2002@gmail.com>
2019-03-17 17:35:04 -0700

    fadsfsa

---
+++ webviewele.js
@@ -0,0 +1,28 @@
+customElements.define('web-view', class extends HTMLElement {  
+    constructor(){
+        super();
+
+        
+
+    }
+
+    connectedCallback(){
+        let response;
+        let ele = this;
+
+        let req = require('https').get(this.getAttribute('src'), (res)=>{
+            let data = "";
+            res.on('data', (d)=>{
+                data+=d;
+            });
+            res.on('end',()=>{
+                console.log(data);
+
+                let shadowRoot = ele.attachShadow({mode: 'open'});
+                shadowRoot.innerHTML = data;
+            });
+
+            
+        });
+    }
+});
