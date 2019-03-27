b60c1ad31cf41e4fae7542e6fcdad4f8530ba3ee
eatmyvenom <jacobhagan@outlook.com>
2019-03-26 21:03:46 -0700

    yaw yeet... so added the read icons.json

--- scripts/readIcons.js
+++ scripts/readIcons.js
@@ -1,10 +1,9 @@
-module.exports = class {
+module.exports = class IconSet {
     constructor(name){
         this.__dir = name;
         
-        var obj = JSON.parse(require('fs').readFileSync('locations.json'));
+        var obj = JSON.parse(require('fs').readFileSync('locations.json').toString());
         this.dir = obj[name];
+        this.icons = JSON.parse(require('fs').readFileSync(dir+'/icons.json').toString());
     }
-
-    
 }
