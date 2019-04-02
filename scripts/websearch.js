204d171db81a14f5f2c43dc2686bf7496465ef86
eatmyvenom <jacobhagan@outlook.com>
2019-04-01 21:15:54 -0700

    this may work

--- scripts/websearch.js
+++ scripts/websearch.js
@@ -1,3 +1,18 @@
-class WebSearch{
-    
+function urlBreadcrumbs(url){
+    var a = url.substring(url.indexOf('/',8)).split('/');
+    a.shift();
+    return a;
 }
+
+class WebSearch{
+    constructor(url){
+        this.crumbs = urlBreadcrumbs(url);
+        this.url = url; 
+    }
+
+    htmlify(){
+        return url.substring(0,url.indexOf('/')) + `<span hint>${this.crumbs.join('/')}</span>`;
+    }
+}
+
+module.exports = WebSearch;
