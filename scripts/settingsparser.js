4d22809c210851b530d581d728f5f8dd080e5e98
eatmyvenom <jacobhagan@outlook.com>
2019-04-07 13:48:51 -0700

    i believe all current settings work

--- scripts/settingsparser.js
+++ scripts/settingsparser.js
@@ -8,7 +8,27 @@ function editSettings(setting,value){
 
 function fontWeight(){
     editSettings('font-weight', $.querySelector('#weight').value);
-    
+}
+
+function ZoomIncrement(){
+    $.querySelector('#incrementV').innerHTML = $.querySelector('#increment').value;
+    editSettings('ZoomIncrement', $.querySelector('#increment').value / 100);
+}
+
+function ZoomLevel(){
+    $.querySelector('#zlvlV').innerHTML = $.querySelector('#zlvl').value + '%';
+    editSettings('ZoomIncrement', $.querySelector('#zlvl').value / 100);
+}
+
+function iconPack(){
+    console.log(this);
+    editSettings('iconPack', this.value);
 }
 
 $.querySelector('#weight').addEventListener('change',fontWeight);
+$.querySelector('#increment').addEventListener('input',ZoomIncrement);
+$.querySelector('#zlvl').addEventListener('input',ZoomLevel);
+
+$.querySelectorAll('#icpp').forEach(e=>{
+    e.addEventListener('click', iconPack);
+});
