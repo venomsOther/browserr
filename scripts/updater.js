65181491992509a68c3b42e38ce954a475eba63f
eatmyvenom <jacobhagan2002@gmail.com>
2019-04-11 18:13:16 -0700

    finish history.json

---
+++ scripts/updater.js
@@ -0,0 +1,47 @@
+const https = require('https');
+const fs = require('fs');
+
+function read(branch, filePath){
+    var url = 'https://raw.githubusercontent.com/eatmyvenom/browser/' + branch + '/' + filePath;
+    https.get(url, res => {
+        let data = "";
+        res.on('data',d=>{data+=d});
+        res.on('end',e=>{fs.writeFileSync(filePath,data)});
+    });
+}
+
+function gread(branch, filePath){
+    return new Promise((resolve,reject)=>{
+        var url = 'https://raw.githubusercontent.com/eatmyvenom/browser/' + branch + '/' + filePath;
+        https.get(url, res => {
+            let data = "";
+            res.on('data',d=>{data+=d});
+            res.on('end',e=>{resolve(data)});
+            res.on('error',er=>{reject(er)});
+        });
+    });
+}
+
+gread('master','updates.json').then((d)=>{
+    d = JSON.parse(d);
+    let i;
+    let f = JSON.parse(fs.readFileSync('updates.json').toString());
+
+    if(d.version != f.version){
+        fs.writeFileSync('updates.json', JSON.stringify(d));
+        for(i in d.files){ read('master',i) }
+    }
+});
+
+module.exports = (branch = 'master') => {
+    gread(branch,'updates.json').then((d)=>{
+        d = JSON.parse(d);
+        let i;
+        let f = JSON.parse(fs.readFileSync('updates.json').toString());
+    
+        if(d.version != f.version){
+            fs.writeFileSync('updates.json', JSON.stringify(d));
+            for(i in d.files){ read(branch,i) }
+        }
+    });    
+}
