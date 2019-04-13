923f7076681e27bd5ccbcbacaa41b29064cd64b5
eatmyvenom <jacobhagan@outlook.com>
2019-04-13 08:20:41 -0700

    some revamps to bookmarks

--- scripts/elements/add-mark.js
+++ scripts/elements/add-mark.js
@@ -1,3 +1,30 @@
+const io = require('../bookmarks.js');
+
+function queryTitle(){
+    return new Promise((resolve,reject)=>{
+        var e = document.createElement('full-query');
+        e.setAttribute('msg','Please enter a title for your bookmark');
+        e.setAttribute('title','Title');
+        document.querySelector('body').appendChild(e);
+
+        e.querySelector('input').addEventListener('keydown',(k)=>{
+            if(k.key == 'enter'){
+                resolve(e.querySelector('input').value);
+            }
+        });
+
+        e.querySelector('done').addEventListener('click',()=>{
+            resolve(e.querySelector('input').value);
+        });
+
+    });
+}
+
+function refreshBookmarks(){
+    document.querySelector('book-marks').connectedCallback();
+}
+
+
 module.exports = class extends HTMLElement{
     constructor(){
         super();
@@ -10,6 +37,18 @@ module.exports = class extends HTMLElement{
     clickEvent(){
         var icon = window.document.querySelector('web--view:not([style="display: none;"])').tab.querySelector('tb-icon img').src;
         var url = window.document.querySelector('web--view:not([style="display: none;"])').view.src;
-        var title;
+        queryTitle().then((title)=>{
+            var t = title;
+            var obj = {
+                href:url,
+                title:t,
+                icon:icon
+            }
+
+            io.set(t,obj);
+            refreshBookmarks();
+        }).catch(e=>{
+            throw new Error(e);
+        });
     }
 }
--- scripts/elements/book-marks.js
+++ scripts/elements/book-marks.js
@@ -1,6 +1,20 @@
+const io = require('../bookmarks.js');
+
 module.exports = class extends HTMLElement {    
     constructor(){
         super();
 
+    }
+
+    connectedCallback(){
+        this.innerHTML = "";
+        var marks = io.readAsObj;
+
+        for(let i in marks){
+            var m = document.createElement('b-mark');
+            m.setAttribute("item",i);
+            
+            this.appendChild(m);
+        }
     }
 }
