d70160d46f12843aec7c069aeef440a8c2639081
eatmyvenom <jacobhagan@outlook.com>
2019-04-15 08:10:25 -1000

    iconStuff

--- scripts/elements/z-in.js
+++ scripts/elements/z-in.js
@@ -2,14 +2,15 @@ var svgs = require('../icons.js');
 const settings = JSON.parse(require('fs').readFileSync('scripts/settings.json').toString());
 const zoomFactorChange = settings.ZoomIncrement;
 
+const readIcons = require('../readIcons.js');
+const IconSet = new readIcons(settings.iconPack);
+
 module.exports = class extends HTMLElement {    
     constructor(){
         super();
         this.addEventListener('click',this.clickEvent);
 
-        let shadowRoot = this.attachShadow({mode:'open'});
-
-        shadowRoot.innerHTML = svgs.smallPlus;
+        this.innerHTML = `<img src="${IconSet.getDir('zoomIn')}" />`;
     }
 
     clickEvent(){
--- scripts/elements/z-out.js
+++ scripts/elements/z-out.js
@@ -2,14 +2,15 @@ var svgs = require('../icons.js');
 const settings = JSON.parse(require('fs').readFileSync('scripts/settings.json').toString());
 const zoomFactorChange = settings.ZoomIncrement;
 
+const readIcons = require('../readIcons.js');
+const IconSet = new readIcons(settings.iconPack);
+
 module.exports = class extends HTMLElement {    
     constructor(){
         super();
         this.addEventListener('click',this.clickEvent);
 
-        let shadowRoot = this.attachShadow({mode:'open'});
-
-        shadowRoot.innerHTML = svgs.smallMinus;
+        this.innerHTML = `<img src="${IconSet.getDir('zoomOut')}" />`;
     }
 
     clickEvent(){
