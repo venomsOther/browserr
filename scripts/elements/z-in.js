9cfc9d1a21277de52aefb50c7e4a44a835f0b5ee
eatmyvenom <jacobhagan@outlook.com>
2019-04-14 14:27:08 -1000

    zoom fixes

--- scripts/elements/z-in.js
+++ scripts/elements/z-in.js
@@ -1,16 +1,18 @@
 var svgs = require('../icons.js');
+const settings = JSON.parse(require('fs').readFileSync('scripts/settings.json').toString());
+const zoomFactorChange = settings.ZoomIncrement;
 
 module.exports = class extends HTMLElement {    
     constructor(){
         super();
-        this.addEventListener('click',this.action);
+        this.addEventListener('click',this.clickEvent);
 
         let shadowRoot = this.attachShadow({mode:'open'});
 
         shadowRoot.innerHTML = svgs.smallPlus;
     }
 
-    action(){
+    clickEvent(){
         getCurrentView().getZoomFactor(factor=>{
             getCurrentView().setZoomFactor(factor + zoomFactorChange);
         });
