bd4324b8391992bd89ba2de9bd34bc9c497e1e51
eatmyvenom <jacobhagan2002@gmail.com>
2019-05-03 20:04:19 -0700

    fix for other things

--- scripts/elements.js
+++ scripts/elements.js
@@ -14,26 +14,50 @@ function preprocess(filename,replacements){
     return txt;
 }
 
-var darkgray = "hsl(0,0%,80%)";
-var lightgrey = "hsl(0,0%,90%)";
-var lighter = "hsl(0,0%,100%)";
-var tabRight = "hsl(0,0%,70%)";
-var black = "hsl(0,0%,50%)";
-var bright = 'white';
+function doTheme(theme){
+
+    var darkgray = "hsl(0,0%,80%)";
+    var lightgrey = "hsl(0,0%,90%)";
+    var lighter = "hsl(0,0%,100%)";
+    var tabRight = "hsl(0,0%,70%)";
+    var black = "hsl(0,0%,50%)";
+    var bright = 'white';
+
+    if(theme == 'bright'){
+        bright = "hsl(0,0%,80%)";
+        black = "hsl(0,0%,90%)";
+        tabRight = "hsl(0,0%,100%)";
+        lighter = "hsl(0,0%,70%)";
+        lightgrey = "hsl(0,0%,50%)";
+        darkgray = 'white';
+    }
+
+    if(theme == 'dark'){
+        darkgray = "hsl(0,0%,20%)";
+        lightgrey = "hsl(0,0%,10%)";
+        lighter = "hsl(0,0%,0%)";
+        tabRight = "hsl(0,0%,30%)";
+        black = "hsl(0,0%,50%)";
+        bright = 'black';
+    }
 
 
-var normalStyle = document.createElement('style');
-normalStyle.innerHTML = preprocess('style.css',{
-    "darkgray":darkgray,
-    "darkgrey":darkgray,
-    "lightgrey":lightgrey,
-    "lightgray":lightgrey,
-    "rgb(220,220,220)":lighter,
-    "rgb(50,50,50)":tabRight,
-    "black":black,
-    "#ffffff":bright
-});
+    var normalStyle = document.createElement('style');
+    normalStyle.innerHTML = preprocess('style.css',{
+        "darkgray":darkgray,
+        "darkgrey":darkgray,
+        "lightgrey":lightgrey,
+        "lightgray":lightgrey,
+        "rgb(220,220,220)":lighter,
+        "rgb(50,50,50)":tabRight,
+        "black":black,
+        "#ffffff":bright,
+        "white":bright,
+        "whitesmoke":bright
+    });
+
+    document.querySelector('head').appendChild(normalStyle);
+}
 
 
-
-document.querySelector('head').appendChild(normalStyle);
+doTheme('b');
