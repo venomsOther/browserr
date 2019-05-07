853c3abc0fac41f2ec8514e8aa922e626e4c350c
eatmyvenom <jacobhagan2002@gmail.com>
2019-05-06 18:13:27 -0700

    pkg updates

--- scripts/updater.js
+++ scripts/updater.js
@@ -26,12 +26,34 @@ function gread(branch, filePath){
     });
 }
 
+function pkgUpdate(url){
+    if(require('fs').readFileSync(__dirname + '/ulock').toString() == 'true')
+    {
+        var filePath = process.execPath.replace(/\\/g,'/');
+        var rename = requrie('fs').renameSync;
+        var del = require('fs').unlinkSync;
+        var write = require('fs').writeFileSync;
+
+        require('https').get(url,res=>{
+            var data = '';
+            write(__dirname + '/ulock','true');
+            res.on('data',d=>data+=d);
+            res.on('end',()=>{
+                write(__dirname + '/ulock','');
+                rename(filePath,filePath + '.old');
+                write(filePath,data);
+                del(filePath + '.old');
+            });
+        });
+    }
+}
+
 function update (branch = 'master') {
 
     if(!require('fs').existsSync(__dirname+'/ulock')){
         require('fs').writeFileSync(__dirname+'/ulock',"");
         require('fs').writeFileSync(__dirname+'/ulock',`{\n\t"version" : "1.1.2",\n\t"files" : \n\t{\n\t\t"scripts/updater.js":"",\n\t\t"scripts/ulock"\n\t},\n\n\t"patches":\n\t{\n\t}\n}`);
-        delete require.cache[require.resolve('./updater.js')]
+        delete require.cache[require.resolve('./updater.js')];
         update(branch);
     }
 
@@ -40,7 +62,7 @@ function update (branch = 'master') {
         d = JSON.parse(d);
         let i;
         let f = JSON.parse(fs.readFileSync(__dirname+'/'+'updates.json').toString());
-        let currentPatch = Object.keys(f.patches).length;
+        let currentPatch = Object.keys(f.patches).length | 0;
 
         if(d.patches[currentPatch + 1] != undefined){
             f.patches[currentPatch + 1] = d.patches[currentPatch + 1];
@@ -56,10 +78,18 @@ function update (branch = 'master') {
              }
             update(branch);
         }
+        else if(d.electronVersion != f.electronVersion){
+            f.electronVersion = d.electronVersion;
+            f.electronUrl = d.electronUrl;
+            fs.writeFileSync(__dirname+'/updates.json', JSON.stringify(f));
+            
+            pkgUpdate(f.electronUrl);
+
+        }
+
     }).catch((e)=>{
-        alert(e);
+        window.alert(e);
         process.exit(0);
-    });
-}
+    });}
 
 module.exports = update;
